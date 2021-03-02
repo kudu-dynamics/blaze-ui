b4f5c9f9ec5af6f512881fbadffce82e7bc5c825
Kevin Orr <kevino@kududyn.com>
2021-03-01 19:01:26 -0500

    Working ICFG

--- binja_plugin/cfg.py
+++ binja_plugin/cfg.py
@@ -1,4 +1,6 @@
-from typing import Dict, Any
+from typing import Dict, Any, Tuple, Optional
+from pathlib import Path
+import json
 
 from binaryninja import PluginCommand, HighlightStandardColor, log_info, log_error, log_warn, BinaryView
 from binaryninjaui import UIAction
@@ -11,129 +13,69 @@ from binaryninjaui import FlowGraphWidget, ViewType
 from binaryninja.plugin import BackgroundTaskThread
 from binaryninja.interaction import show_graph_report
 
-example_graph = {
-    'nodes': [
-        {
-            'id': 1,
-            'lines': [
-                'Branch cond: cmpE (rcx) (0)'
-            ],
-        },
-        {
-            'id': 2,
-            'lines': [
-                'rdi = rcx', \
-                'call f f [0xdeadbeef]', \
-                'enter context?',
-            ],
-        },
-        {
-            'id': 3,
-            'lines': [
-                'rax = mul (2) (rdi)',
-            ],
-        },
-        {
-            'id': 4,
-            'lines': [
-                'exit context?', \
-                'rcx = rax',
-            ],
-        },
-        {
-            'id': 5,
-            'lines': [
-                'rax = rcx',
-            ],
-        }
-    ],
-    'edges': [
-        {
-            'from': 1,
-            'to': 5,
-            'type': 'true',
-        },
-        {
-            'from': 1,
-            'to': 2,
-            'type': 'false',
-        },
-        {
-            'from': 2,
-            'to': 3,
-            'type': 'call',
-        },
-        {
-            'from': 3,
-            'to': 4,
-            'type': 'return',
-        },
-        {
-            'from': 4,
-            'to': 5,
-            'type': 'unconditional',
-        }
-    ]
-}
-
-EDGE_TYPES = {
-    'true': (BranchType.TrueBranch, None),
-    'false': (BranchType.FalseBranch, None),
-    'unconditional': (BranchType.UnconditionalBranch, None),
-    'call': (BranchType.UserDefinedBranch,
-             EdgeStyle(style=EdgePenStyle.DashLine,
-                       theme_color=ThemeColor.UnconditionalBranchColor)),
-    'return': (BranchType.UserDefinedBranch,
-               EdgeStyle(style=EdgePenStyle.DotLine,
-                         theme_color=ThemeColor.UnconditionalBranchColor)),
-}
+ENTER_FUNC_TAG = 'EnterFunc'
+LEAVE_FUNC_TAG = 'LeaveFunc'
+BASIC_BLOCK_TAG = 'BasicBlock'
 
 
-def show_cfg(cfg: Dict[str, Any] = example_graph):
+def fixup_graph(graph):
+    graph = {**graph, 'nodeMap': {k: v for [k, v] in graph['nodeMap']}}
+    return graph
+
+
+with open(Path(__file__).parent / 'res' / 'cfg.json') as f:
+    BEFORE_GRAPH = fixup_graph(json.load(f))
+
+with open(Path(__file__).parent / 'res' / 'cfg_pruned.json') as f:
+    AFTER_GRAPH = fixup_graph(json.load(f))
+
+
+def get_edge_type(edge, nodes) -> Tuple[BranchType, Optional[EdgeStyle]]:
+    node_from = nodes[edge['src']]
+    node_to = nodes[edge['dst']]
+
+    if node_to['tag'] == ENTER_FUNC_TAG:
+        assert edge['branchType'] == 'UnconditionalBranch', 'bad assumption'
+        return (BranchType.UserDefinedBranch,
+                EdgeStyle(style=EdgePenStyle.DashLine,
+                          theme_color=ThemeColor.UnconditionalBranchColor))
+
+    if node_from['tag'] == LEAVE_FUNC_TAG:
+        assert edge['branchType'] == 'UnconditionalBranch', 'bad assumption'
+        return (BranchType.UserDefinedBranch,
+                EdgeStyle(style=EdgePenStyle.DotLine,
+                          theme_color=ThemeColor.UnconditionalBranchColor))
+
+    return {
+        'TrueBranch': (BranchType.TrueBranch, None),
+        'FalseBranch': (BranchType.FalseBranch, None),
+        'UnconditionalBranch': (BranchType.UnconditionalBranch, None),
+    }[edge['branchType']]
+
+
+def format_block_header(node_id: int, node: dict) -> str:
+    tag = node['tag']
+    start_addr = node["contents"].get("start", None)
+    if tag == BASIC_BLOCK_TAG and start_addr is not None:
+        return f'#{node_id} ({start_addr:#x}) {node["tag"]}'
+
+
+def show_cfg(cfg: Dict[str, Any] = BEFORE_GRAPH):
     graph = FlowGraph()
     nodes = {}
 
-    for node in cfg['nodes']:
+    for (node_id, node) in cfg['nodeMap'].items():
         fg_node = FlowGraphNode(graph)
-        fg_node.lines = node['lines']
-        nodes[node['id']] = fg_node
+
+        fg_node.lines = [format_block_header(node_id, node)]
+        if (nodeData := node.get('contents', {}).get('nodeData', None)):
+            fg_node.lines += nodeData
+
+        nodes[node_id] = fg_node
         graph.append(fg_node)
 
     for edge in cfg['edges']:
-        branch_type, edge_style = EDGE_TYPES[edge['type']]
-        nodes[edge['from']].add_outgoing_edge(branch_type, nodes[edge['to']],
-                                              edge_style)
+        branch_type, edge_style = get_edge_type(edge, cfg['nodeMap'])
+        nodes[edge['src']].add_outgoing_edge(branch_type, nodes[edge['dst']],
+                                             edge_style)
     show_graph_report("Graph", graph)
-
-# Basic sample flowgrpah
-#
-# Creates a flow graph, showing some basic functionality
-
-def start_cfg(bv, func):
-    graph = FlowGraph()
-    node_a = FlowGraphNode(graph)
-    node_a.lines = ["Node A"]
-    node_b = FlowGraphNode(graph)
-    node_b.lines = ["Node B"]
-    node_c = FlowGraphNode(graph)
-    node_c.lines = ["Node C"]
-    graph.append(node_a)
-    graph.append(node_b)
-    graph.append(node_c)
-    node_a.add_outgoing_edge(BranchType.UnconditionalBranch, node_b)
-    node_a.add_outgoing_edge(BranchType.UnconditionalBranch, node_c)
-    show_graph_report("In order", graph)
-
-    graph2 = FlowGraph()
-    node2_a = FlowGraphNode(graph)
-    node2_a.lines = ["Node A"]
-    node2_b = FlowGraphNode(graph)
-    node2_b.lines = ["Node B"]
-    node2_c = FlowGraphNode(graph)
-    node2_c.lines = ["Node C"]
-    graph2.append(node2_b)
-    graph2.append(node2_c)
-    graph2.append(node2_a)
-    node2_a.add_outgoing_edge(BranchType.UnconditionalBranch, node2_b)
-    node2_a.add_outgoing_edge(BranchType.UnconditionalBranch, node2_c)
-    show_graph_report("Out of order", graph)
