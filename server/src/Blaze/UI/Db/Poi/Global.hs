c517d19438e4d8a80854f3bebf25c65e6ca73538
mpdairy <mp@kududyn.com>
2022-02-18 16:04:50 -0700

    upsert

--- server/src/Blaze/UI/Db/Poi/Global.hs
+++ server/src/Blaze/UI/Db/Poi/Global.hs
@@ -10,24 +10,6 @@ import Data.Time.Clock (getCurrentTime)
 import Blaze.UI.Types.Poi (Poi(Poi), poiTable)
 import Blaze.UI.Types.BinaryHash (BinaryHash)
 
-poiExists
-  :: MonadDb m
-  => BinaryHash
-  -> Address
-  -> Bytes
-  -> Maybe Text
-  -> Maybe Text
-  -> m Bool
-poiExists binHash funcAddr instrOffset poiName poiDescription = fmap (not . null) . withDb . query $ do
-  poi <- select poiTable
-  restrict $ poi ! #binaryHash .== literal binHash
-         .&& poi ! #isGlobalPoi .== literal True
-         .&& poi ! #funcAddr .== literal funcAddr
-         .&& poi ! #instrAddr .== literal (funcAddr + fromIntegral instrOffset)
-         .&& poi ! #name .== literal poiName
-         .&& poi ! #description .== literal poiDescription
-         .&& poi ! #isGlobalPoi .== literal True
-  return poi
 
 saveNew
   :: MonadDb m
@@ -37,24 +19,31 @@ saveNew
   -> Maybe Text
   -> Maybe Text
   -> m ()
-saveNew binHash funcAddr instrOffset poiName poiDescription = do
-  exists <- poiExists binHash funcAddr instrOffset poiName poiDescription
-  unless exists . withDb $ do
-    pid <- liftIO randomIO
-    utc <- liftIO getCurrentTime
-    insert_ poiTable
-      [ Poi
-        pid
-        Nothing
-        Nothing
-        binHash
-        utc
-        funcAddr
-        (funcAddr + fromIntegral instrOffset)
-        poiName
-        poiDescription
-        True
-      ]
+saveNew binHash funcAddr instrOffset poiName poiDescription = withDb $ do
+  pid <- liftIO randomIO
+  utc <- liftIO getCurrentTime
+  void $ upsert poiTable
+    (\poi -> poi ! #binaryHash .== literal binHash
+         .&& poi ! #isGlobalPoi .== literal True
+         .&& poi ! #funcAddr .== literal funcAddr
+         .&& poi ! #instrAddr .== literal (funcAddr + fromIntegral instrOffset)
+         .&& poi ! #name .== literal poiName
+         .&& poi ! #description .== literal poiDescription
+         .&& poi ! #isGlobalPoi .== literal True
+    )
+    identity
+    [ Poi
+      pid
+      Nothing
+      Nothing
+      binHash
+      utc
+      funcAddr
+      (funcAddr + fromIntegral instrOffset)
+      poiName
+      poiDescription
+      True
+    ]
 
 getPoisOfBinary :: MonadDb m => BinaryHash -> m [Poi]
 getPoisOfBinary binHash = withDb . query $ do
